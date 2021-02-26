f6fc83a35a347e933a52d979ae87705a4af5a6e2
MathMan <kayacan.goktug@gmail.com>
2021-02-26 22:48:58 +0300

    Initial port of math graph stuff
    Only linerenderer implementation so far

---
+++ Assets/Prototypes/MathGraph/Readme.md
@@ -0,0 +1,6 @@
+# Search Algorithms
+
+This is a small test script set about node based graph path finding / search algorithms
+
+
+[![IMAGE ALT TEXT HERE](https://github.com/Math-Man/UnityProvingGrounds/blob/main/Assets/Prototypes/SearchAlgorithms/gifs/2.gif)](https://github.com/Math-Man/UnityProvingGrounds/blob/main/Assets/Prototypes/SearchAlgorithms/gifs/2.gif)
---
+++ Assets/Prototypes/MathGraph/Scripts/Grapher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Profiling;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Grapher : MonoBehaviour
+{
+
+    [SerializeField] private int Resolution;
+    [SerializeField] private Vector3 Offset;
+    [SerializeField] private float StepBerth;
+
+    private LineRenderer lineRenderer;
+    private float[] linePoints;
+    
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        linePoints = new float[Resolution];
+
+        float step = StepBerth * 2f / Resolution;
+        var scale = Vector3.one * step;
+        var pos = Vector3.zero;
+        Vector3[] pointPositions = new Vector3[Resolution];
+        for (int i = 0; i < Resolution; i++) 
+        {
+            linePoints[i] = (i + 0.5f) * step - 1f;
+            pointPositions[i] = new Vector3(linePoints[i], 0, 0) + Offset;
+        }
+
+        lineRenderer.positionCount = Resolution;
+        lineRenderer.SetPositions(pointPositions);
+
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Resolution; i++)
+        {
+            float y = Mathf.Sin(Mathf.PI * (linePoints[i] / Mathf.PI + 0.5f * Time.time));
+            y += 0.5f * Mathf.Sin(2f * Mathf.PI * (linePoints[i] / Mathf.PI + Time.time));
+
+
+            lineRenderer.SetPosition(i, new Vector3(linePoints[i], y, 0) + Offset);
+
+            //lineRenderer.SetPosition(i, new Vector3(linePoints[i],
+            //    Mathf.Sin(linePoints[i] / Mathf.PI + Time.time) + Mathf.Sin(1.77f + linePoints[i] / Mathf.PI + Time.time),
+            //    0) + Offset);
+        }
+    }
+}
